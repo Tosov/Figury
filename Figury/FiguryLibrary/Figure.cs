3b1c8feead77c26da3065f1f7d6faa77ce81c98e
Tosov <y.tosov@microsoft.wsei.edu.pl>
2020-03-04 20:17:05 +0100

    cwiczenie

---
+++ Figury/FiguryLibrary/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiguryLibrary
+{
+    abstract public class Figure
+    {
+        public string Color { get; set; } // read-write
+        public Figure(string color = "black")
+        {
+            Color = color;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"figure{this}, color = {Color}");
+        }
+
+        abstract public void Draw(); // resowanie figury za pomoca plottera
+        
+
+        
+
+    }
+}
---
+++ Figury/FiguryLibrary/Triangle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FiguryLibrary
+{
+    /// <summary>
+    /// Trojkat w wersji immutable
+    /// </summary>
+    public class Triangle : Figure
+    {
+        public double A { get; private set; } // A jest read-only
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        public Triangle(double a = 1, double b = 1, double c = 1) : base("red")
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+                throw new ArgumentOutOfRangeException("boki powinny byc dodatnie");
+
+            if (a + b <= c || a + c <= b || b + c <= a)
+                throw new ArgumentException("nie spelniony warunek trojkata");
+
+            A = a;
+            B = b;
+            C = c;
+
+        }
+        public override string ToString()
+        {
+            return $"triangle({A}, {B}, {C})";
+        }
+
+        public double Perimeter => A + B + C;
+
+        public Triangle Scale( double factor )
+        {
+            if (factor < 0)
+                throw new ArgumentOutOfRangeException("wsp. skalowanie musi byc dodatni");
+
+            return new Triangle(A * factor, B * factor, C * factor);
+
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("rysowanie trojkata");
+        }
+    }
+}
